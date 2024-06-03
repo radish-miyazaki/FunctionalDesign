87ff18d416bbff04a8641757bd614d83bfb0918f
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-06-03 18:48:51 +0900

    read Section.3

---
+++ RecursionAndIteration/recursive-fib/test/recursive_fib/core_test.clj
@@ -0,0 +1,7 @@
+(ns recursive-fib.core-test
+  (:require [clojure.test :refer :all]
+            [recursive-fib.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
