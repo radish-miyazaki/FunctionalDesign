17f56ab5790a5a0a29870af090decaba3b5b4b58
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-06-11 23:22:00 +0900

    read up to page 140

---
+++ copy/spec/copy/core_spec.clj
@@ -0,0 +1,55 @@
+(ns copy.core-spec
+  (:require
+   [copy.core :refer :all]
+   [speclj.core :refer [describe it should=]]))
+
+(def str-in (atom nil))
+(def str-out (atom nil))
+
+(defn str-read []
+  (let [c (first @str-in)]
+    (if (nil? c)
+      :eof
+      (do
+        (swap! str-in rest)
+        c))))
+
+(defn str-write [c]
+  (swap! str-out str c)
+  str-write)
+
+;; (describe "copy"
+;;           (it "can read and write str-read and str-write"
+;;               (reset! str-in "abcdef")
+;;               (reset! str-out "")
+;;               (copy str-read str-write)
+;;               (should= "abcdef" @str-out)))
+
+;; (describe "copy"
+;;           (it "can read and write str-read and str-write"
+;;               (reset! str-in "abcdef")
+;;               (reset! str-out "")
+;;               (copy {:getchar str-read :putchar str-write})
+;;               (should= "abcdef" @str-out)))
+
+(describe "copy"
+          (it "can read and write str-read and str-write"
+              (let [device {:device-type :test-device
+                            :input (atom "abcdef")
+                            :output (atom nil)}]
+                (copy device)
+                (should= "abcdef" @(:output device)))))
+
+(defmethod getchar :test-device [device]
+  (let [input (:input device)
+        c (first @input)]
+    (if (nil? c)
+      :eof
+      (do
+        (swap! input rest)
+        c))))
+
+(defmethod putchar :test-device [device c]
+  (let [output (:output device)]
+    (swap! output str c)))
+
---
+++ copy/src/copy/core.clj
@@ -0,0 +1,29 @@
+(ns copy.core)
+
+;; (defn copy [read write]
+;;   (let [c (read)]
+;;     (if (= c :eof)
+;;       nil
+;;       (recur read (write c)))))
+
+;; (defn copy [device]
+;;   (let [c ((:getchar device))]
+;;     (if (= c :eof)
+;;       nil
+;;       (do
+;;         ((:putchar device) c)
+;;         (recur device)))))
+
+(defmulti getchar (fn [device] (:device-type device)))
+(defmulti putchar (fn [device _] (:device-type device)))
+
+(defn copy [device]
+  (let [c (getchar device)]
+    (if (= c :eof)
+      nil
+      (do
+        (putchar device c)
+        (recur device)))))
+
+
+
