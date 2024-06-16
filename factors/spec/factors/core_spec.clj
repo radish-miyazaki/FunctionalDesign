6ae8a90d5a73726d0b9c7700f3cfc2bfdf87b642
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-06-16 17:18:07 +0900

    add sample test target func

--- factors/spec/factors/core_spec.clj
+++ factors/spec/factors/core_spec.clj
@@ -1,7 +1,34 @@
 (ns factors.core-spec
-  (:require [speclj.core :refer :all]
-            [factors.core :refer :all]))
+  (:require
+   [factors.core :refer :all]
+   [speclj.core :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(defn power2 [n]
+  (apply * (repeat n 2)))
+
+(describe "factor primes"
+ (it "factors 1 -> []"
+  (should= [] (factors-of 1)))
+ (it "factors 2 -> [2]"
+  (should= [2] (factors-of 2))
+ (it "factors 3 -> [3]"
+  (should= [3] (factors-of 3)))
+ (it "factors 4 -> [2 2]"
+  (should= [2 2] (factors-of 4)))
+ (it "factors 5 -> [5]"
+  (should= [5] (factors-of 5)))
+ (it "factors 6 -> [2 3]"
+  (should= [2 3] (factors-of 6)))
+ (it "factors 7 -> [7]"
+  (should= [7] (factors-of 7)))
+ (it "factors 8 -> [2 2 2]"
+  (should= [2 2 2] (factors-of 8)))
+ (it "factors 9 -> [3 3]"
+  (should= [3 3] (factors-of 9)))
+ (it "factors lots"
+   (should= (* 2 2 3 5 7 11 11 13) (factors-of (* 2 2 3 5 7 11 11 13))))
+ (it "factors Euler 3"
+  (should= [71 839 1471 6857] (factors-of 600851475143)))
+ (it "factors mersenne 2^31-1"
+  (should= [2147483647] (factors-of (power2 31))))))
+
--- factors/src/factors/core.clj
+++ factors/src/factors/core.clj
@@ -1,5 +1,11 @@
 (ns factors.core)
 
-(defn -main
-  [& args]
-  (println "Hello World"))
+(defn factors-of [n]
+  (loop [factors [] n n divisor 2]
+    (if (> n 1)
+      (cond
+        (> divisor (Math/sqrt n)) (conj factors n)
+        (= 0 (mod n divisor)) (recur (conj factors divisor) (quot n divisor) divisor)
+        :else (recur factors n (inc divisor)))
+      factors)))
+
