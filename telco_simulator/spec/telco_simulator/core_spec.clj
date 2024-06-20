7d8b99b39d06f2c18a0a788c799185a3e1497a3d
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-06-20 16:01:21 +0900

    read up to Section.15

--- telco_simulator/spec/telco_simulator/core_spec.clj
+++ telco_simulator/spec/telco_simulator/core_spec.clj
@@ -14,5 +14,21 @@
      (prn @log)
      (should= :idle (:state @caller))
      (should= :idle (:state @callee))
-     (should= :idle (:state @telco)))))
+     (should= :idle (:state @telco))))
+
+ ;; (it "should race"
+ ;;  (let [caller (make-user-agent "Bob")
+ ;;        callee (make-user-agent "Alice")
+ ;;        telco1 (make-telco-agent "telco1")
+ ;;        telco2 (make-telco-agent "telco2")]
+ ;;    (reset! log [])
+ ;;    (send caller transition :call [telco1 caller callee])
+ ;;    (send callee transition :call [telco2 callee caller])
+ ;;    (Thread/sleep 100)
+ ;;    (prn @log)
+ ;;    (should= :idle (:state @caller))
+ ;;    (should= :idle {:state @callee})
+ ;;    (should= :idle {:state @telco1})
+ ;;    (should= :idle {:state @telco2})))
+ )
 
